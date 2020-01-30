57aefbe1e4ddacd072159de3981a763fb9997fd0
bobrock <barret.obrock@gmail.com>
2020-01-30 13:27:40 -0600

    Remove Blueprint object from flask scripts as conflicted with Slack, add copy of service file

--- cv/templates/__init__.py
+++ cv/templates/__init__.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from .hello.views import hello_blueprint
-
-app = Flask(__name__, static_folder='./public', template_folder='./static')
-
-# Register the blueprints
-app.register_blueprint(hello_blueprint)
+# from flask import Flask
+# from .hello.views import hello_blueprint
+#
+# app = Flask(__name__, static_folder='./public', template_folder='./static')
+#
+# # Register the blueprints
+# app.register_blueprint(hello_blueprint)
--- cv/templates/hello/views.py
+++ cv/templates/hello/views.py
@@ -1,54 +1,54 @@
-import os
-import dotenv
-from flask import render_template, Blueprint
-from slackeventsapi import SlackEventAdapter
-
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-dotenv.load_dotenv(dotenv_path)
-slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
-
-hello_blueprint = Blueprint('hello', __name__)
-
-
-@hello_blueprint.route('/')
-def index():
-    return render_template('home.html')
-
-
-@hello_blueprint.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@hello_blueprint.route('/cv')
-def cv():
-    return render_template('cv.html')
-
-
-@hello_blueprint.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-
-@hello_blueprint.route('/contact')
-def contact():
-    return render_template('contact.html')
-
-
-@hello_blueprint.route('/tools')
-def tools():
-    return render_template('tools.html')
-
-
-@hello_blueprint.route('/', subdomain='okr')
-def okr_main():
-    return render_template('okr_home.html')
-
-
-sea = SlackEventAdapter(slack_signing_secret, "/vikapi/events", hello_blueprint)
-
-
-@sea.on('reaction_added')
-def reaction_added(event_data):
-    emoji = event_data['event']['reaction']
-    print(emoji)
+# import os
+# import dotenv
+# from flask import render_template, Flask
+# from slackeventsapi import SlackEventAdapter
+#
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# dotenv.load_dotenv(dotenv_path)
+# slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
+#
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def index():
+#     return render_template('home.html')
+#
+#
+# @hello_blueprint.route('/about')
+# def about():
+#     return render_template('about.html')
+#
+#
+# @app.route('/cv')
+# def cv():
+#     return render_template('cv.html')
+#
+#
+# @app.route('/projects')
+# def projects():
+#     return render_template('projects.html')
+#
+#
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
+#
+#
+# @app.route('/tools')
+# def tools():
+#     return render_template('tools.html')
+#
+#
+# @app.route('/', subdomain='okr')
+# def okr_main():
+#     return render_template('okr_home.html')
+#
+#
+# sea = SlackEventAdapter(slack_signing_secret, "/vikapi/events", app)
+#
+#
+# @sea.on('reaction_added')
+# def reaction_added(event_data):
+#     emoji = event_data['event']['reaction']
+#     print(emoji)
