f951507086495c7bd5cf88de1c738290becf709a
bobrock <barret.obrock@gmail.com>
2020-01-30 16:12:36 -0600

    Add method of reading in signing secrets & tokens

--- cv/flask_base.py
+++ cv/flask_base.py
@@ -1,15 +1,16 @@
 import os
-import dotenv
-from flask import render_template, Flask
-from slackeventsapi import SlackEventAdapter
+from flask import render_template, Flask, request, jsonify
 
 
 key_path = os.path.join(os.path.expanduser('~'), 'keys')
 keys = ['viktor', 'cah', 'dnd']
 keys_dict = {}
 for k in keys:
-    with open(os.path.join(key_path, f'{k.upper()}_SLACK_SIGNING_SECRET')) as f:
-        keys_dict[k] = f.read().strip()
+    for t in ['SIGNING_SECRET', 'XOXB_TOKEN', 'XOXP_TOKEN', 'VERIFY_TOKEN']:
+        key_dict = {}
+        with open(os.path.join(key_path, f'{k.upper()}_SLACK_{t}')) as f:
+            key_dict[t.lower()] = f.read().strip()
+    keys_dict[k] = key_dict
 
 app = Flask(__name__, static_folder='./templates/public', template_folder='./templates/static')
 
@@ -49,15 +50,37 @@ def okr_main():
     return render_template('okr_home.html')
 
 
-# Viktor boi
-viktor_events = SlackEventAdapter(keys_dict['viktor'], "/vikapi/events", app)
+# # Viktor boi
+# viktor_events = SlackEventAdapter(keys_dict['viktor'], "/vikapi/events", app)
 # # Wizzy boi
 # cah_events = SlackEventAdapter(cah_sss, "/cahapi/events", app)
 # # DND boi
 # dnd_events = SlackEventAdapter(dnd_sss, "/dndapi/events", app)
 
+@app.route('/vikapi/events', methods=['POST'])
+def viktor_events():
+    if request.form['token'] != keys_dict['viktor']:
+        # Token submitted not the same as what's expected. Don't do anything
+        return
 
-@viktor_events.on('reaction_added')
-def reaction_added(event_data):
-    emoji = event_data['event']['reaction']
-    print(emoji)
+    event_data = request.form['event']
+    if event_data['type'] == 'reaction_added':
+        payload = {
+            'text': f'Test was successful. Reaction detected: {event_data["reaction"]}',
+            'channel': event_data['item']['channel']
+        }
+        return jsonify(payload)
+
+
+@app.route('/cahapi/events', methods=['POST'])
+def cah_events():
+    if request.form['token'] != keys_dict['cah']:
+        # Token submitted not the same as what's expected. Don't do anything
+        return
+
+    event_data = request.form['event']
+    if event_data['type'] == 'reaction_added':
+        payload = {
+            'text': f'Test was successful. Reaction detected: {event_data["reaction"]}'
+        }
+        return jsonify(payload)
